87178761aeb60b633ac1651ca4c9c29f1113c673
AxelLeo129 <axelleo129dev@gmail.com>
2023-08-03 16:22:25 -0600

    Hamming emisor JS

--- javascript_version/index.js
+++ javascript_version/index.js
@@ -1,9 +1,70 @@
+function parity(m) {
+    let r = 1;
+    while (m + r + 1 > Math.pow(2, r)) {
+        r += 1;
+    }
+    return r;
+}
+
+function positionsBinary(length, zeroCount) {
+    const binaries = [];
+    for(let i = 1; i <= length; i++) {
+        let binary = i.toString(2);
+        while(binary.length < zeroCount) {
+            binary = '0' + binary;
+        }
+        binaries.push(binary);
+    }
+    return binaries;
+}
+
+function dOrP(length, trama) {
+    const helper = [];
+    let tramaIndex = 0;
+    let pCount = 0;
+    for (let i = 1; i <= length; i++) {
+        if (Math.log2(i) % 1 === 0) {
+            helper.push(pCount);
+            pCount++;
+        } else {
+            helper.push(trama[tramaIndex]);
+            tramaIndex++;
+        }
+    }
+    return helper;
+}
+
+function countOnes(array) {
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == 1) {
+            count++;
+        }
+    }
+    return count;
+}
 function submit() {
-    const code = document.getElementById('code').value;
-    const data = document.getElementById('data').value;
-    const parity = document.getElementById('parity').value;
+    const trama = document.getElementById('code').value;
+    const paridad = parity(trama.length);
+    const longitudMensaje = paridad + trama.length;
+    const posicionesBinarias = positionsBinary(longitudMensaje, paridad);
+    const nuevaTrama = dOrP(longitudMensaje, trama);
+    const operaciones = [];
+    const tramaFinal = [...nuevaTrama];
+    nuevaTrama.forEach((element, index) => {
+        if (typeof element === 'number') {
+            const operacion = nuevaTrama.map((flement, jndex) => {
+                if (typeof flement === 'string') {
+                    const elementoBinario = posicionesBinarias[jndex].split('').reverse().join('');
+                    return elementoBinario[element] == 1 ? flement : ' ';
+                }
+                return ' ';
+            });
+            operacion[index] = countOnes(operacion) % 2 === 0 ? '0' : '1';
+            operaciones.push(operacion);
+            tramaFinal[index] = operacion[index];
+        }
+    });
+    document.getElementById('response').innerHTML = `<p>Respuesta: ${tramaFinal.join('')}</p>`;
 
-    
-
-    console.log(code, data, parity);
 }
