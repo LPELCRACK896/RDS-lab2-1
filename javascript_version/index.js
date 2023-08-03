6421451c511e0db754efcba746656bca6f7404c0
AxelLeo129 <axelleo129dev@gmail.com>
2023-08-03 12:28:15 -0600

    Project JS structure

---
+++ javascript_version/index.js
@@ -0,0 +1,9 @@
+function submit() {
+    const code = document.getElementById('code').value;
+    const data = document.getElementById('data').value;
+    const parity = document.getElementById('parity').value;
+
+    
+
+    console.log(code, data, parity);
+}
