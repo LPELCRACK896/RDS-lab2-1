04ba7ecf482b9a1162c6c217ab8e2982440954d6
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-08-11 20:23:54 -0600

    evaluate hamming

--- python_version/main.py
+++ python_version/main.py
@@ -9,6 +9,76 @@ HOST = '127.0.0.1'
 PORT = 8080
 
 
+def evaluate_transmission_crc(data):
+
+    print(1) 
+    pass
+
+def decode_hamming_after_noise(trama, is_altered):
+    msg_concat, was_error_detected, was_error_corrected = hamming_receptor(trama)
+    pass
+
+def evaluate_transmission_hamming(data, chance_of_flipping_bit = 0.01):
+    errores_detectados = 0
+    total_errores = 0
+    errores_corregidos = 0
+    palabras_correctas = 0
+    palabras_intactas = 0
+
+    TP, FP, TN, FN = 0, 0, 0, 0 # Para hacer matriz de confusion
+
+
+    for word_data_recieved in data:
+        WordValuesList = word_data_recieved['binaryValuesList']
+        WordProccesed = word_data_recieved['binaryValuesListProccesed']
+        WordProccesedWithNoise, is_altered = add_noise_word(WordProccesed, chance_of_flipping=chance_of_flipping_bit)
+        for char, was_altered in WordProccesedWithNoise:
+            decoded_msg, was_error_detected, was_error_corrected = hamming_receptor(char[0])
+
+            if is_altered: # Si hubo ruido en la trama
+                if was_error_detected:
+                    if was_error_corrected:
+                        TP += 1
+                    else:
+                        FN += 1
+                else:
+                    FN += 1
+            else: 
+                if was_error_detected:
+                    FP += 1
+                else:
+                    TN += 1
+    # Cálculo de métricas
+    try:
+        precision = TP / (TP + FP)
+    except ZeroDivisionError:
+        precision = 0
+
+    try:
+        recall = TP / (TP + FN)
+    except ZeroDivisionError:
+        recall = 0
+
+    try:
+        f1_score = 2 * (precision * recall) / (precision + recall)
+    except ZeroDivisionError:
+        f1_score = 0
+
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    # Print the confusion matrix
+    print("Matriz de confusión para la codificación Hamming:")
+    print(f"TP: {TP}, FP: {FP}, TN: {TN}, FN: {FN}")
+    print("Métricas para la codificación Hamming:")
+    print(f"Precisión: {precision:.2f}, Exhaustividad: {recall:.2f}, Puntaje F1: {f1_score:.2f}, Exactitud: {accuracy:.2f}")
+
+
+    
+
+    print(1) 
+
+
+    pass
+
 def add_noise_trama(binary, chance_of_flipping):
     new_binary = ""
     altered = False
@@ -21,10 +91,10 @@ def add_noise_trama(binary, chance_of_flipping):
 
     return new_binary, altered
 
-def add_noise_data(binaryValuesListProccesed, chance_of_flipping = 0.01):
+def add_noise_word(binaryValueListProccesed, chance_of_flipping = 0.01):
     binaryValuesListProccesedPostNoise = []
     is_word_altered = False
-    for char in binaryValuesListProccesed:
+    for char in binaryValueListProccesed:
         new_word = []
         is_char_altered = False
         for bit in char:
@@ -80,7 +150,13 @@ def main():
         s.listen()
         
         data = get_data(s, 100)
-        binaryValuesListProccesedPostNoise, is_word_altered = add_noise_data(data[0]["binaryValuesListProccesed"], chance_of_flipping=0.01)
+        method = data[0]["methodName"]
+
+        if method == "hamming":
+            evaluate_transmission_hamming(data)
+        else: # crc
+            evaluate_transmission_crc(data)
+
         print("All messages processed!")
 
 if __name__ == "__main__":
